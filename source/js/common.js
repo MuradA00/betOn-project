be8b9ddba5539dbc48669497e928e8b522929af0
Murad <abishlimurad@gmail.com>
2023-01-13 02:10:13 +0200

    sidebar adaptive etc

--- source/js/common.js
+++ source/js/common.js
@@ -1,12 +1,22 @@
 const header = document.querySelector('.header'),
       sidebar = document.querySelector('.sidebar'),
-      content = document.querySelector('.grid-wrapper__inner');
+      footer = document.querySelector('.footer'),
+      content = document.querySelector('.grid-wrapper__inner'),
+      tabletMedia = window.matchMedia('(max-width: 650px)');
 
 function setHeadersPosition() {
   const sidebarWidth = sidebar.clientWidth;
   const headerHeight = header.clientHeight;
+
   content.style.marginTop = `${headerHeight}px`;
-  header.style.width = `calc(100% - ${sidebarWidth}px)`;
+
+  if (sidebar) {
+    getComputedStyle(sidebar).position === 'fixed' ? header.style.width = '100%' : header.style.width = `calc(100% - ${sidebarWidth}px)`;
+  }
+  if (tabletMedia.matches && sidebar) {
+    const sidebarHeight = sidebar.clientHeight;
+    footer.style.marginBottom = `${sidebarHeight}px `
+  }
 }
 
 
