337912961ada370ce5c4f823844d36413e75905a
Murad <abishlimurad@gmail.com>
2023-01-17 07:53:09 +0200

    burger menu

--- source/js/common.js
+++ source/js/common.js
@@ -2,7 +2,11 @@ const header = document.querySelector('.header'),
       sidebar = document.querySelector('.sidebar'),
       footer = document.querySelector('.footer'),
       content = document.querySelector('.grid-wrapper__inner'),
-      tabletMedia = window.matchMedia('(max-width: 650px)');
+      tabletMedia = window.matchMedia('(max-width: 650px)'),
+      burger = document.querySelector('.header__burger'),
+      menu = document.querySelector('.menu'),
+      closeIcon = document.querySelector('.menu__close'),
+      body = document.querySelector('.grid-wrapper__content')
 
 function setHeadersPosition() {
   const sidebarWidth = sidebar.clientWidth;
@@ -17,6 +21,34 @@ function setHeadersPosition() {
     const sidebarHeight = sidebar.clientHeight;
     footer.style.marginBottom = `${sidebarHeight}px `
   }
+}
+
+function showMenu() {
+  closeIcon.classList.remove('closed')
+  burger.classList.toggle('active-burger');
+  body.classList.add('body-locked')
+  if (burger.classList.contains('active-burger')) {
+    menu.classList.add('show-menu')
+    body.classList.add('body-locked')
+  } else {
+    menu.classList.remove('show-menu')
+    body.classList.remove('body-locked');
+  }
+}
+
+function closeMenu() {
+  closeIcon.classList.add('closed');
+  menu.classList.remove('show-menu');
+  burger.classList.remove('active-burger');
+  body.classList.remove('body-locked');
+}
+
+if (burger) {
+  closeIcon.addEventListener('click', closeMenu);
+}
+
+if (burger) {
+  burger.addEventListener('click', showMenu);
 }
 
 setHeadersPosition();
